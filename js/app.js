c1d413475448e78c9255acba034d5374854cf979
ianfshirley <ianfshirley88@gmail.com>
2022-10-25 23:28:31 -0700

    added 3rd & 4th random x position

--- js/app.js
+++ js/app.js
@@ -10,7 +10,10 @@ function setRandomXPosition() {
   root.style.setProperty('--first-x-position', newFirstX + 'px');
   let newSecondX = getRandomNum(0, 320);
   root.style.setProperty('--second-x-position', newSecondX + 'px'); 
-  //TODO: set the rest of the x positions
+  let newThirdX = getRandomNum(400, 720);
+  root.style.setProperty('--third-x-location', newThirdX + 'px');
+  let newFourthX = getRandomNum(0, 320);
+  root.style.setProperty('--fourth-x-postion', newFourthX + 'px');
 }
 
 root.addEventListener('animationiteration', () => {
